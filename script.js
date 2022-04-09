b81809e2212692776aa97eebe110372c830a178a
WiktorIdziak <Witusaki@gmail.com>
2022-04-09 04:43:55 +0200

    fix adress v2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://tasks-mr.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'https://api.trello.com/1/trello';
+  const trelloApiRoot = 'https://api.trello.com/1';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
